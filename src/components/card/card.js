83b6220b7af2570e622c246c07a4d0c685f34231
Mudit Sharma <muditshar001@gmail.com>
2024-06-17 21:33:06 +0530

    Update card.js

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,43 +1,42 @@
-import React, {useEffect, useState} from "react"
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
-import "./card.css"
-import { Link } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "./card.css";
+import { Link } from "react-router-dom";
 
-const Cards = ({movie}) => {
-
-    const [isLoading, setIsLoading] = useState(true)
+const Cards = ({ movie }) => {
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         setTimeout(() => {
-            setIsLoading(false)
-        }, 1500)
-    }, []) 
+            setIsLoading(false);
+        }, 1500);
+    }, []);
 
-    return <>
-    {
-        isLoading
-        ?
-        <div className="cards">
-            <SkeletonTheme color="#202020" highlightColor="#444">
-                <Skeleton height={300} duration={2} />
-            </SkeletonTheme>
-        </div>
-        :
-        <Link to={`/movie/${movie.id}`} style={{textDecoration:"none", color:"white"}}>
-            <div className="cards">
-                <img className="cards__img" src={`https://image.tmdb.org/t/p/original${movie?movie.poster_path:""}`} />
-                <div className="cards__overlay">
-                    <div className="card__title">{movie?movie.original_title:""}</div>
-                    <div className="card__runtime">
-                        {movie?movie.release_date:""}
-                        <span className="card__rating">{movie?movie.vote_average:""}<i className="fas fa-star" /></span>
-                    </div>
-                    <div className="card__description">{movie ? movie.overview.slice(0,118)+"..." : ""}</div>
+    return (
+        <>
+            {isLoading ? (
+                <div className="cards">
+                    <SkeletonTheme color="#202020" highlightColor="#444">
+                        <Skeleton height={300} duration={2} />
+                    </SkeletonTheme>
                 </div>
-            </div>
-        </Link>
-    }
-    </>
-}
+            ) : (
+                <Link to={`/movie/${movie.imdbID}`} style={{ textDecoration: "none", color: "white" }}>
+                    <div className="cards">
+                        <img className="cards__img" src={movie.Poster !== "N/A" ? movie.Poster : "placeholder_image_url"} alt={movie.Title} />
+                        <div className="cards__overlay">
+                            <div className="card__title">{movie ? movie.Title : ""}</div>
+                            <div className="card__runtime">
+                                {movie ? movie.Released : ""}
+                                <span className="card__rating">{movie ? movie.imdbRating : ""}<i className="fas fa-star" /></span>
+                            </div>
+                            <div className="card__description">{movie ? movie.Plot.slice(0, 118) + "..." : ""}</div>
+                        </div>
+                    </div>
+                </Link>
+            )}
+        </>
+    );
+};
 
-export default Cards
+export default Cards;
